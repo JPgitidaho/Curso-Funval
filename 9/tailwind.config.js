9ff71a1ce24ca6064f9875f48bb54325ac96edd7
JPgitidaho <per22030@byui.edu>
2025-05-29 17:03:00 -0400

    actualization

---
+++ 9/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  darkMode: 'class', 
+  content: [
+    './index.html',  
+    './scrip.js',
+    './9/index/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
